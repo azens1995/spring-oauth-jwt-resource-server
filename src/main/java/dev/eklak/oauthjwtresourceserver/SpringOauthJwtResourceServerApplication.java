382507dff9ce47c5af02c6bbe82bfbea9b95e525
Eklak Dangaura <azens1995@gmail.com>
2023-09-19 11:57:07 +0545

    feat: add jwt symmetric key implementation for oauth

--- src/main/java/dev/eklak/oauthjwtresourceserver/SpringOauthJwtResourceServerApplication.java
+++ src/main/java/dev/eklak/oauthjwtresourceserver/SpringOauthJwtResourceServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringOauthJwtResourceServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringOauthJwtResourceServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauthJwtResourceServerApplication.class,
+            args);
+    }
 }
--- src/main/java/dev/eklak/oauthjwtresourceserver/controllers/HelloController.java
+++ src/main/java/dev/eklak/oauthjwtresourceserver/controllers/HelloController.java
@@ -1,2 +1,13 @@
-package dev.eklak.oauthjwtresourceserver.controllers;public class HelloController {
+package dev.eklak.oauthjwtresourceserver.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!!";
+    }
 }
